4c15f57ddecf9ad5db34ac24a3afe9993e27cc1e
admin123 <office@163.com>
2023-04-03 11:07:00 +0800

    Compatible processing of HTTP_CONTENT_TYPE received in Nginx and APACHE

--- libs/core/http/HttpRequest.php
+++ libs/core/http/HttpRequest.php
@@ -13,10 +13,13 @@ class HttpRequest
     public static function getAllHeaders()
     {
         foreach ($_SERVER as $key => $value){
+//            nginx中的响应头和apache有些地方是有些去别的，比如：HTTP_CONTENT_TYPE nginx中带有HTTP但是 apache则不带PHP 需要做兼容
             if(substr($key,0,5) == 'HTTP_'){
                 $headers[str_replace(' ','-',strtolower(str_replace('_',' ',substr($key,5))))] = $value;
             }
+
         }
+        if(array_key_exists('content-type',$headers) == false) $headers['content-type'] = $_SERVER['CONTENT_TYPE'];
         $headers['method'] = $_SERVER['REQUEST_METHOD'];
         return $headers;
     }
