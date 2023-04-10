7ad48954066a20cd6cdaed378491b3955f198bfe
admin123 <office@163.com>
2023-04-10 11:50:41 +0800

    LDAP login initialization, configuration, matching according to cn and password

--- app/Controller/api/IndexController.php
+++ app/Controller/api/IndexController.php
@@ -8,6 +8,7 @@
 
 namespace app\Controller\api;
 
+use app\Controller\common\Ldap;
 use app\Controller\common\RedisCache;
 use libs\core\Cache\Cache;
 
@@ -16,17 +17,24 @@ class IndexController
     public function index()
     {
 //        Redis测试
-       $redisTest =  new  RedisCache();
-       $redisTest->getRedisInstance();
+//       $redisTest =  new  RedisCache();
+//       $redisTest->getRedisInstance();
 //        缓存文件测试 新增文件缓存类Cache
-        $data = array(
-            'name'=>'shenguan',
-            'age' => 1,
-        );
-        $cache = new Cache();
-        $cache->set("name", "wanghaiyang");
-        $cache->set("company", $data);
-        var_dump( $cache->get('name'));
-        var_dump( $cache->get('company'));
+//        $data = array(
+//            'name'=>'shenguan',
+//            'age' => 1,
+//        );
+//        $cache = new Cache();
+//        $cache->set("name", "wanghaiyang");
+//        $cache->set("company", $data);
+//        var_dump( $cache->get('name'));
+//        var_dump( $cache->get('company'));
+//        ldapc测试
+        //连接LDAP服务器
+        $password = 'admin';
+        $cn = 'cheng';
+        $LdapService = new Ldap();
+        $result = $LdapService->getLdapUserinfo($cn,$password);
+        var_dump($result);
     }
 }
